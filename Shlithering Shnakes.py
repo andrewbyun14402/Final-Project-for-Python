e1e39d667b030e942d2b46acdf59b63d25ee1cbf
andrewbyun14402 <81570266+andrewbyun14402@users.noreply.github.com>
2021-04-16 11:48:02 -0400

    Update Shlithering Shnakes.py

--- Shlithering Shnakes.py
+++ Shlithering Shnakes.py
@@ -1,4 +1,19 @@
 #Shlithering Shnakes
 #By Andrew Byun, Lang Li, Vraj Patel
+import turtle
 
+#Screen
+wn=turtle.Screen()
+wn.title("Shlithering Shnakes")
+wn.bgcolor("black")
+wn.setup(width=600,height=600)
+wn.tracer(0)
 
+#Shnake
+shnake=turtle.Turtle()
+shnake.speed(0)
+shnake.shape("square")
+shnake.color("green")
+shnake.penup()
+shnake.goto(0,0)
+shnake.direction="stop"
