9923e4c0d708f9104871e08a7da0e5d4a896607c
andrewbyun14402 <81570266+andrewbyun14402@users.noreply.github.com>
2021-04-21 18:32:31 -0400

    Update Shlithering Shnakes.py

--- Shlithering Shnakes.py
+++ Shlithering Shnakes.py
@@ -109,7 +109,7 @@ while True:
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
 
     # Check for a collision with the food
-    if head.distance(food1) < 20:
+    if shnake.distance(food1) < 20:
         # Move the food to a random spot
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
